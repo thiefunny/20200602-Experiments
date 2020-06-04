f9ca5ce12dfc3c80c66c21dcf427c363781fac31
thiefunny <mikolaj@walanus.com>
2020-06-04 22:02:37 +0200

    finalna wersja spiral

--- app6.js
+++ app6.js
@@ -1,10 +1,9 @@
 const canvasEl = document.querySelector("#drawing");
 const ctx = canvasEl.getContext('2d');
-const canvasWidth = 500;
-const canvasHeight = 500;
+const canvasWidth = window.innerWidth/1.2;
+const canvasHeight = window.innerHeight/1.5;
 const canvasBorder = canvasHeight / 2;
 const scoreEl = document.querySelector(".score");
-
 
 canvasEl.style.borderRadius = canvasBorder + "px";
 canvasEl.setAttribute("width", canvasWidth + "px");
@@ -15,11 +14,14 @@ let x = canvasWidth / 2,
     r = 0,
     a = 1,
     cr = 200,
-    
     cg = 200,
     cb = 200,
     rstep = 0.1,
-    rdiv = 10
+    rdiv = 10,
+    // speed = document.querySelector("#speed").value,
+    speed = 30;
+    r2 = 1;
+    r3 = 0.1;
 
 function circle(x, y, r, cr, cg, cb, a) {
 
@@ -28,6 +30,7 @@ function circle(x, y, r, cr, cg, cb, a) {
     ctx.arc(x, y, r, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.closePath();
+    
 }
 
 setInterval(function () {
@@ -35,14 +38,14 @@ setInterval(function () {
         a = document.getElementById("a").value;
         rstep = document.querySelector("#rstep").value;
         rdiv = document.querySelector("#rdiv").value;
+        r3 = document.querySelector("#r3").value;
+        r2 += Number(r3);
         r += Number(rstep);
         x += r / Number(rdiv) * Math.sin(r);
         y += r / Number(rdiv) * Math.cos(r);
         cr = r * 3.6;
         cg = 100;
         cb = 50;
-        circle(x, y, r, cr, cg, cb, a);
-        // scoreEl.innerHTML = a;
-        // scoreEl.innerHTML = rstep;
+        circle(x, y, r2, cr, cg, cb, a);
     }
-}, 5);
+}, speed);
