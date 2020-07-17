afea2b710d3cd82bce12213d786afb2a5febb92e
Mikołaj Walanus <mikolaj@walanus.com>
2020-07-17 15:38:54 +0200

    kalk 1.3 jeszcze działa jako podstawowa wersja

--- 16.js
+++ 16.js
@@ -3,12 +3,14 @@ const liczba2El = document.querySelector("#liczba2");
 const operationTypeEl = document.querySelector("#operationtype");
 const buttonScoreEl = document.querySelector("#score");
 const buttonAddFieldEl = document.querySelector("#addfield");
+const digitInputEl = document.querySelectorAll(".digitsinput");
+const ulEL = document.querySelector(".inputs");
 
 let Score = {
-    sum: function() {
+    sum: function () {
         return this.liczba1a + this.liczba2a;
     },
-    multiply: function() {
+    multiply: function () {
         return this.liczba1a * this.liczba2a;
     }
 }
@@ -17,15 +19,22 @@ function wynik() {
     Score.liczba1a = Number(liczba1El.value);
     Score.liczba2a = Number(liczba2El.value);
     Score.operationType = operationTypeEl.value;
-    switch(Score.operationType) {
+    switch (Score.operationType) {
         case "+":
             console.log(Score.sum());
             break;
-            
+
         case "*":
             console.log(Score.multiply());
             break;
     }
 }
 
-buttonScoreEl.addEventListener("click", wynik)
+function addField() {
+    let newField = document.createElement("input");
+    // newField.classList.add("digitsinput");
+    ulEL.appendChild(newField);
+}
+
+buttonScoreEl.addEventListener("click", wynik);
+buttonAddFieldEl.addEventListener("click", addField);
