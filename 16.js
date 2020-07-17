273e6316b92d02814fd86389d0bbd32e28fc40ba
Mikołaj Walanus <mikolaj@walanus.com>
2020-07-17 16:12:08 +0200

    kalk 1.9 do poprawy wygląd jeszcze

--- 16.js
+++ 16.js
@@ -3,29 +3,45 @@ const liczba2El = document.querySelector("#liczba2");
 const operationTypeEl = document.querySelector("#operationtype");
 const buttonScoreEl = document.querySelector("#score");
 const buttonAddFieldEl = document.querySelector("#addfield");
-const digitInputEl = document.querySelectorAll(".digitsinput");
 const ulEL = document.querySelector(".inputs");
 
-let Score = {
+let Operation = {
     sum: function () {
-        return this.liczba1a + this.liczba2a;
-    },
+        i = 0;
+        for (elem of Score) {
+            i += elem;
+        }
+        return i;
+        },
+        
     multiply: function () {
-        return this.liczba1a * this.liczba2a;
+        i = 1;
+        for (elem of Score) {
+            i *= elem;
+        }
+        return i;
     }
-}
+};
+
+let Score = [];
 
 function wynik() {
-    Score.liczba1a = Number(liczba1El.value);
-    Score.liczba2a = Number(liczba2El.value);
-    Score.operationType = operationTypeEl.value;
-    switch (Score.operationType) {
+    let digitInputEl = document.querySelectorAll("input");
+    let i = 0;
+    for (elem of digitInputEl) {
+        Score[i] = Number(elem.value);
+        i++;
+    }
+
+    Operation.operationType = operationTypeEl.value;
+    
+    switch (Operation.operationType) {
         case "+":
-            console.log(Score.sum());
+            console.log(Operation.sum());
             break;
 
         case "*":
-            console.log(Score.multiply());
+            console.log(Operation.multiply());
             break;
     }
 }
