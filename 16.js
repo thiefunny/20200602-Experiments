cc3e096ec022a131e1d3a6ec4dd1ed54e96128bf
Mikołaj Walanus <mikolaj@walanus.com>
2020-07-17 12:31:10 +0200

    kalk 0.3

--- 16.js
+++ 16.js
@@ -1,40 +1,48 @@
 const liczba1El = document.querySelector("#liczba1");
 const liczba2El = document.querySelector("#liczba2");
+const buttonEl = document.querySelector("button");
 
 let Score = {
     sum: function() {
-        // return this.liczba1 + this.liczba2;
-    },
-    multiply: function() {
-        return liczba1a * liczba2a;
-    },
+        return this.liczba1 + this.liczba2;
+    }
+    // multiply: function() {
+    //     return liczba1a * liczba2a;
+    // },
 }
 
 liczba1El.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        let liczba1a = liczba1El.value;
-        console.log(liczba1a);
+        Score.liczba1a = liczba1El.value;
+        console.log(Score.liczba1a);
     }
 })
 
 liczba2El.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        let liczba2a = liczba2El.value;
-        console.log(liczba2a);
-        // console.log(Score.sum());
-        
+        Score.liczba2a = liczba2El.value;
+        console.log(Score.liczba2a);
     }
-    
 })
 
-window.addEventListener("keypress", function () {
-    
+// buttonEl.onclick = function() {
+//     // liczba1a = liczba1El.value;    liczba2a = liczba2El.value;
+
+//     console.log(`--- ${liczba1a}`);
+//         console.log(`--- ${liczba2a}`);
+// }
+
+document.addEventListener("keypress", function (event) {
+    if (event.key === "w") {
+        console.log(`--- ${Score.liczba1a}`);
+        console.log(`--- ${Score.liczba2a}`);
+        console.log(Score.sum())
+    }
 })
 
-console.log(`--- ${liczba1a}`);
-console.log(`--- ${liczba2a}`);
 
-console.log(Score.multiply());
+
+// console.log(Score.multiply());
 // let liczba1a = 20;
 // let liczba2a = 30;
 
