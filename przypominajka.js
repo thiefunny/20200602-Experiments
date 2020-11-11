b802c62acb17a3a6c5d2ba0456aa890146f0c46a
thiefunny <mikolaj@walanus.com>
2020-11-11 13:38:02 +0100

    przypominajka 3

--- przypominajka.js
+++ przypominajka.js
@@ -1,4 +1,6 @@
 const inner = document.querySelector("p");
+const innerDwa = document.querySelector("#dwa");
+
 
 const witek = {
     "imie": "Witek",
@@ -8,7 +10,14 @@ const witek = {
         function sum() {
             return x + y
         }
-        inner.innerHTML = text + sum();}
+        inner.innerHTML = text + sum();},
+
+        "liczenieDwa": function wszystko(text, x, y) {
+
+            function sum() {
+                return x + y
+            }
+            innerDwa.innerHTML = text + sum();}
 
         // if (x > 0) {Inner.innerHTML = sum}
         // else {Inner.innerHTML = "kupa"};
@@ -27,6 +36,16 @@ witek.liczenie("psik: ", 10, 5);
 
 const liczby = [];
 
-for (let i = 0; i < 10; i++) {liczby.push(i); console.log(liczby[i]);}
-console.log(liczby);
+// for (let i = 0; i < 10; i++) {liczby.push(i); console.log(liczby[i]);}
+// console.log(liczby);
 
+
+for (let i = 0; i < 10; i++) {witek.liczenieDwa("psik: ", i, i*2)};
+
+let arr = [1,2,3,4,5,6,7,8]
+console.log('arr: ' + arr);
+let arrCopy = arr.slice();
+console.log("arrCopy: " + arrCopy);
+
+// innerDwa.style.color = "#f00";
+innerDwa.classList.add("zielony");
---
+++ przypominajka_todoist.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector("input");
+const tasksEl = document.querySelector(".todoist__task__group");
+
+
+
+inputEl.focus();
+inputEl.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        let task = document.createElement("li");
+        tasksEl.appendChild(task);
+        task.innerHTML = inputEl.value;
+        inputEl.value = '';
+    }
+})
