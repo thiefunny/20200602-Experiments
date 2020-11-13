bf8bd43c33a44a7c3fca522531c2dc8b2c5dd1a6
thiefunny <mikolaj@walanus.com>
2020-11-13 14:33:52 +0100

    przypominajka - skończona

--- przypominajka_menu.js
+++ przypominajka_menu.js
@@ -1,8 +1,21 @@
 const mainEl = document.querySelector("main");
 const bodyEl = document.querySelector("body");
 const runEl = document.getElementById("run");
+// const timerEl = document.getElementById("timer");
+const todoistEl = document.getElementById("todoist");
+const sectionEl = document.querySelectorAll("section");
 
 runEl.addEventListener("click", function() {
     mainEl.classList.toggle("center");
 })
 
+mainEl.addEventListener("click", function(event) {
+
+for (elem of sectionEl) {
+    elem.onclick = function() {
+        // console.log(this);
+        this.classList.toggle("short");
+    }
+    // event.target.classList.toggle("short");
+}
+})
