17ee4ec8d0dd97abd202ba47b7edc74bf7dd015c
Mikolaj Walanus <mikolaj@walanus.com>
2020-11-16 14:05:56 +0100

    przypominajka - obiekty kolejne

--- przypominajka_object4.js
+++ przypominajka_object4.js
@@ -1,16 +1,30 @@
 let Fish = function (gatunekarg, wielkoscarg) {
     this.gatunek = gatunekarg,
-    this.wielkosc = wielkoscarg,
-    this.show = function () {
-        return `${this.gatunek} ${this.wielkosc}`
-    }
+    this.wielkosc = wielkoscarg
 }
 
+Fish.prototype.show = function () {
+    return `${this.gatunek} ${this.wielkosc}`
+    }
+
+let arr = new Array;
+let variable, fishvar;
+
+for (i = 0; i < 10; i++) {
+    let j = Math.round(Math.random() * 100);
+    fish = new Fish(`ryba${i}`, j)
+    arr.push(fish);
+}
+
+for (elem of arr) {
+    // console.log(elem);
+    console.log(elem.show());
+}
+
+// console.log(arr[1].show());
+
+// arr[3]
+
+// console.log(arr[3])
 
 
-
-// let fish1 = new Fish('miki', 'mała');
-// let fish2 = new Fish('wiki', 'duza');
-
-// console.log(fish1.show())
-// console.log(fish2.show())
