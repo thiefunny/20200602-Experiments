bd091b0c312965d10dd928afa939c4b2e80b650b
Mikolaj Walanus <mikolaj@walanus.com>
2020-11-27 11:24:54 +0100

    mistrz callbackow

--- 22_callbacks.js
+++ 22_callbacks.js
@@ -1,15 +1,92 @@
-function randomIntegerBetweenZeroAnd(max) {
-	return Math.floor((Math.random() * max) + 1);
-}
+const talkDelay = () => Math.floor(Math.random()*1000)+1000;
+const talkLongDelay = 4000;
 
-function someApiRequest(apiName) {
-  var responseTime = randomIntegerBetweenZeroAnd(4000);
-  
-  setTimeout( function(){
-    console.log("response from: ", apiName);
-  }, randomIntegerBetweenZeroAnd(responseTime) );
-}
+const arr = ['- Cześć Zosia', '- Cześć Adam, co tam?', '- Chuj Ci w dupę.', '- ... jak zawsze uprzejmy, a tak na serio to co chciałeś?', `- ... długo jeszcze będę czekać na odpowiedź? Minęło już ${talkLongDelay} milisekund...`, `- ... aaa whatever, pa Adaś...`, '- Aha, i chuj Ci w dupę też.'];
 
-someApiRequest("twitter")
-someApiRequest("google")
-someApiRequest("facebook")
+////////// rozmowa typ 4 async callbacks
+
+let indexArr = -1;
+
+const setIndex = _ => indexArr += 1;
+
+const talk = (callback) => setTimeout(_ => callback(arr[setIndex()]), talkDelay());
+const talkLong = (callback) => setTimeout(_ => callback(arr[setIndex()]), talkLongDelay);
+
+
+const print = arg => console.log(arg);
+
+talk((elem) => {
+    print(elem);
+    talk((elem) => {
+        print(elem);
+        talk((elem) => {
+            print(elem);
+            talk((elem) => {
+                print(elem);
+                talkLong((elem) => {
+                    print(elem);
+                    talkLong((elem) => {
+                        print(elem);
+                    })
+                })
+            })
+        })
+    })
+})
+
+
+
+////////// rozmowa typ 3 async callbacks
+
+// const funcA = (callback) => setTimeout(_ => callback(arr[0]), miliseconds)
+// const funcB = (callback) => setTimeout(_ => callback(arr[1]), miliseconds)
+// const funcC = (callback) => setTimeout(_ => callback(arr[2]), miliseconds)
+// const funcD = (callback) => setTimeout(_ => callback(arr[3]), miliseconds)
+
+// const print = arg => console.log(arg);
+
+// funcA((elem) => {
+//     print(elem);
+//     funcB((elem) => {
+//         print(elem);
+//         funcC((elem) => {
+//             print(elem);
+//             funcD((elem) => {
+//                 print(elem)
+//             })
+//         })
+//     })
+// })
+
+
+
+////////// rozmowa typ 2 callbacks
+
+// let indexArr = -1;
+
+// const setIndex = _ => indexArr += 1;
+
+// const print = (index) => {console.log(arr[index])}
+
+// const dialog = (callback) => {
+//     callback(setIndex());
+// }
+
+// for (i=0; i<arr.length; i++) {dialog(print);}
+
+
+
+
+
+
+////////// rozmowa typ 1
+
+/* let x = 0
+
+let y = setInterval(_ => {
+    console.log(arr[x])
+    x += 1
+    if (x > 4) {
+        clearInterval(y)
+    }
+}, 1000) */
