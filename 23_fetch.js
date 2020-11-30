8226b4acfe7dc01a71b2f1d148792ffcc67e5baa
Mikolaj Walanus <mikolaj@walanus.com>
2020-11-30 18:46:51 +0100

    fetch 2

--- 23_fetch.js
+++ 23_fetch.js
@@ -1,7 +1,54 @@
+const content = document.querySelector('p');
+
+const URL_END_POINT = "https://api.themoviedb.org/3/"
+const SEARCH_TYPE = `keyword/7376/movies`
+const URL_API_KEY = `?api_key=db59a7858683873689962abfe2676567&query=`
+const SEARCH_QUERY = `&page=1`
+const SEARCH_QUERY_PAGE = `&page=`
+
 // console.log(fetch('https://opentdb.com/api.php?amount=10'))
 
-fetch('https://opentdb.com/api.php?amount=10')
-    .then(response => response.json())
-    .then(result => {
-        console.log(result.results[0].question)
-    })
+const URL = `${URL_END_POINT}${SEARCH_TYPE}${URL_API_KEY}${SEARCH_QUERY}`;
+const URL2 = `${URL_END_POINT}${SEARCH_TYPE}${URL_API_KEY}${SEARCH_QUERY_PAGE}`;
+
+let moviesArr = [];
+
+const populateArr = _ => {
+
+    return fetch(URL)
+        .then(response => response.json())
+        .then(result => {
+            for (let i = 1; i < result.total_pages + 1; i++) {
+                fetch(`${URL2}${i}`)
+                    .then(response => response.json())
+                    .then(result => {
+                        result.results.forEach(movie => moviesArr.push(movie))
+                        return moviesArr
+                    })
+            }
+            return new Promise((resolve) => {
+                resolve(moviesArr)
+            })
+        })
+        .then(result => {
+            console.log(result.length)
+            console.log(result)
+            console.log(moviesArr.length)
+        })
+
+}
+
+
+// const print = elem => {console.log(elem)}
+
+
+populateArr();
+
+let mymovie = {
+    title: '',
+    image: ''
+};
+moviesArr.forEach(movie => {
+    mymovie.title = movie.title;
+    mymovie.image = movie.poster_path;
+})
